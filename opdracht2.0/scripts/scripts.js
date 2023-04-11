6e1be726acb70701a6ba7d1c6a2868a6a918c09d
Isabel Kok <isabelkok2004@gmail.com>
2023-04-11 17:27:38 +0200

    .

--- opdracht2.0/scripts/scripts.js
+++ opdracht2.0/scripts/scripts.js
@@ -26,22 +26,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-var optionAll = document.querySelector("#filter-all");
-var optionWater = document.querySelector("#filter-wateranimal");
-var optionLand = document.querySelector("#filter-landanimals");
-var optionPets = document.querySelector("#filter-pets");
-
-
-optionAll.addEventListener("change", filterList);
-optionWater.addEventListener("change", filterList);
-optionLand.addEventListener("change", filterList);
-optionPets.addEventListener("change", filterList);
-
-
-function filterList(event){
-  let deLijst = document.querySelector("div");
-  let nieuweFilter = event.target.value;
-  deLijst.className = "";
-  deLijst.classList.add(nieuweFilter);
-}
