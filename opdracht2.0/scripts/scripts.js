2021ff858cf6cb7f589db1a3d7e03077da9e1e90
Isabel-Kok <isabelkok2004@gmail.com>
2023-04-28 15:10:47 +0200

    hihi

--- opdracht2.0/scripts/scripts.js
+++ opdracht2.0/scripts/scripts.js
@@ -1,28 +1,52 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// JavaScript Document
+console.log("howdy");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+var optionAll = document.querySelector("#filter-all");
+var optionWater = document.querySelector("#filter-wateranimal");
+var optionLand = document.querySelector("#filter-landanimals");
+var optionPets = document.querySelector("#filter-pets");
+
+
+optionAll.addEventListener("change", filterList);
+optionWater.addEventListener("change", filterList);
+optionLand.addEventListener("change", filterList);
+optionPets.addEventListener("change", filterList);
+
+
+function filterList(event){
+  let deLijst = document.querySelector("ul");
+  let nieuweFilter = event.target.value;
+  deLijst.className = "";
+  deLijst.classList.add(nieuweFilter);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+	
+	autoplay: {
+		 delay: 1000,
+		disableOnInteraction: true,
+	 },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -22,31 +22,5 @@ function filterList(event){
 }
 
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-	
-	autoplay: {
-		 delay: 1000,
-		disableOnInteraction: true,
-	 },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
 
 
