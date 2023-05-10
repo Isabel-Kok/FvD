f9ee3d44fdac0ddfe2a2e85fb8c72f83ebb17cfd
Isabel-Kok <isabelkok2004@gmail.com>
2023-05-10 19:59:33 +0200

    hihi

--- opdracht2.0/scripts/scripts.js
+++ opdracht2.0/scripts/scripts.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 console.log("howdy");
 
+var autoplayIsOn = true;
+
 
 var optionAll = document.querySelector("#filter-all");
 var optionWater = document.querySelector("#filter-wateranimal");
@@ -15,23 +17,40 @@ optionPets.addEventListener("change", filterList);
 
 
 function filterList(event){
-  let deLijst = document.querySelector("ul");
+  let deLijst = document.querySelector("section");
   let nieuweFilter = event.target.value;
   deLijst.className = "";
   deLijst.classList.add(nieuweFilter);
 }
 
+var playpauseButton = document.querySelector("main>button");
 
+playpauseButton.addEventListener("click", playpause);
+
+function playpause(){
+  if(autoplayIsOn) {
+    swiper.autoplay = false;
+    // swiper.autoplay.stop();
+    autoplayIsOn = false;
+    console.log("stop");
+  } else {
+    // swiper.autoplay = 1000;
+    swiper.autoplay.start();
+    autoplayIsOn = true;
+    console.log("start");
+  }
+  
+}
 
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: false,
+  loop: true,
 	
 	autoplay: {
-		 delay: 1000,
-		disableOnInteraction: true,
+		delay: 1000,
+		disableOnInteraction: false,
 	 },
 
   // If we need pagination
@@ -45,8 +64,9 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
+  scrollbar:false,
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
